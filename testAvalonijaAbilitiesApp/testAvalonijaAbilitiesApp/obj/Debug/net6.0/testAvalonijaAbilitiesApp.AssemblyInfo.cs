2f22c8b488abc3f33037e0145b575420fd25ccc9
Keingar <99558524+Keingar@users.noreply.github.com>
2024-11-27 21:23:32 +0200

    idk

--- testAvalonijaAbilitiesApp/testAvalonijaAbilitiesApp/obj/Debug/net6.0/testAvalonijaAbilitiesApp.AssemblyInfo.cs
+++ testAvalonijaAbilitiesApp/testAvalonijaAbilitiesApp/obj/Debug/net6.0/testAvalonijaAbilitiesApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("testAvalonijaAbilitiesApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+023c9e5dc7b3e74333fc5a8fd1b59eafea096137")]
 [assembly: System.Reflection.AssemblyProductAttribute("testAvalonijaAbilitiesApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("testAvalonijaAbilitiesApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
